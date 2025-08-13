80066f5c16b6880689595f10823b963a35a56a8e
rubanofficial <rubans082005@gmail.com>
2025-08-13 16:08:40 +0530

    Time: 6 ms (16.45%), Space: 42.6 MB (6.86%) - LeetHub

--- 1954-replace-all-digits-with-characters/1954-replace-all-digits-with-characters.java
+++ 1954-replace-all-digits-with-characters/1954-replace-all-digits-with-characters.java
@@ -1,5 +1,15 @@
 class Solution {
     public String replaceDigits(String s) {
-        
+        char [] a = s.toCharArray();
+        String b = "";
+        for(int i=0;i<s.length();i++){
+            if(i%2 == 0){
+                b = b + a[i];
+            }
+            else{
+                b = b + (char)(a[i-1] + a[i] - '0');
+            }
+        }
+        return b;
     }
 }
